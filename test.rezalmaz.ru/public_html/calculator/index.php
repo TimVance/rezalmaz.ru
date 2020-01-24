8a0a5d8e96ac3f196bbc1d289943bf1848b45ad5
TimVance <2DngxvshA6GUGcQ>
2020-01-25 02:12:24 +0300

    Страница калькулятора

--- test.rezalmaz.ru/public_html/calculator/index.php
+++ test.rezalmaz.ru/public_html/calculator/index.php
@@ -6,7 +6,7 @@ $APPLICATION->SetPageProperty("description", "Калькулятор алмаз�
 if(!CModule::IncludeModule("iblock"))
  return;
 
-$calc_groups = CIBlockElement::GetList(Array("SORT"=>"ASC"), Array("IBLOCK_ID" => 5, "ACTIVE" => "Y"));
+$calc_groups = CIBlockElement::GetList(Array("SORT"=>"ASC"), Array("IBLOCK_ID" => 3, "ACTIVE" => "Y"));
 while($calc_group = $calc_groups->GetNextElement()){
 	$arr_group = $calc_group->GetFields();
 	$group_id = $arr_group['ID'];
@@ -18,7 +18,7 @@ while($calc_group = $calc_groups->GetNextElement()){
 	}
 }
 
-$calc_types = CIBlockElement::GetList(Array("SORT"=>"ASC"), Array("IBLOCK_ID" => 4, "ID" => $arr_types_list, "ACTIVE" => "Y"));
+$calc_types = CIBlockElement::GetList(Array("SORT"=>"ASC"), Array("IBLOCK_ID" => 2, "ID" => $arr_types_list, "ACTIVE" => "Y"));
 while($calc_type = $calc_types->GetNextElement()){
 	$arr_type = $calc_type->GetFields();
 	$type_id = $arr_type['ID'];
@@ -56,17 +56,17 @@ while($calc_type = $calc_types->GetNextElement()){
 				<div name="<?=$group_id?>" class="stage_2">
 					<div class="stage">
 						<div class="column_short">
-							<?switch($group_id){
-								case 5374:
-									$sTempStr = 'резать';
-									break;
-								case 5375:
-									$sTempStr = 'сверлить';
-									break;
-								case 5376:
-									$sTempStr = 'штробить';
-									break;
-							}?>
+                            <?switch($group_id){
+                                case 17:
+                                    $sTempStr = 'резать';
+                                    break;
+                                case 18:
+                                    $sTempStr = 'сверлить';
+                                    break;
+                                case 19:
+                                    $sTempStr = 'штробить';
+                                    break;
+                            }?>
 							<div class="text_head"><div class="step_index">2.</div>Что будем <?=$sTempStr?> ?</div>
 						</div>
 						<div class="column">
@@ -81,7 +81,7 @@ while($calc_type = $calc_types->GetNextElement()){
 					</div>
 				</div>
 			<?php endforeach;?>
-			<div work="5374" name="param" class="stage_3">
+			<div work="17" name="param" class="stage_3">
 				<div class="stage">
 					<div class="column_short">
 						<div class="text_head"><div class="step_index">3.</div>Укажите параметры:</div>
@@ -89,17 +89,17 @@ while($calc_type = $calc_types->GetNextElement()){
 					<div class="column">
 						<div class="sel_param_row">
 							<span class="label_param">Толщина материала (мм.)</span>
-<!-- 							<input type="text" class="changed_param" id="5374_thick" value="0"> -->
-							<select class="changed_param" id="5374_thick"></select>
+<!-- 							<input type="text" class="changed_param" id="17_thick" value="0"> -->
+							<select class="changed_param" id="17_thick"></select>
 						</div>
 						<div class="sel_param_row">
 							<span class="label_param">Длина реза (м.)</span>
-							<input type="text" class="changed_param" id="5374_length" value="0">
+							<input type="text" class="changed_param" id="17_length" value="0">
 						</div>
 					</div>
 				</div>
 			</div>
-			<div work="5375" name="param" class="stage_3">
+			<div work="18" name="param" class="stage_3">
 				<div class="stage">
 					<div class="column_short">
 						<div class="text_head"><div class="step_index">3.</div>Укажите параметры:</div>
@@ -107,21 +107,21 @@ while($calc_type = $calc_types->GetNextElement()){
 					<div class="column">
 						<div class="sel_param_row">
 							<span class="label_param">Диаметр отверстия (мм.)</span>
-<!-- 							<input type="text" class="changed_param" id="5375_diameter" value="0"> -->
-							<select class="changed_param" id="5375_diameter"></select>
+<!-- 							<input type="text" class="changed_param" id="18_diameter" value="0"> -->
+							<select class="changed_param" id="18_diameter"></select>
 						</div>
 						<div class="sel_param_row">
 							<span class="label_param">Глубина сверления (см.)</span>
-							<input type="text" class="changed_param" id="5375_depth" value="0">
+							<input type="text" class="changed_param" id="18_depth" value="0">
 						</div>
 						<div class="sel_param_row">
 							<span class="label_param">Колличество отверстий</span>
-							<input type="text" class="changed_param" id="5375_count" value="0">
+							<input type="text" class="changed_param" id="18_count" value="0">
 						</div>
 					</div>
 				</div>
 			</div>
-			<div work="5376" name="param" class="stage_3">
+			<div work="19" name="param" class="stage_3">
 				<div class="stage">
 					<div class="column_short">
 						<div class="text_head"><div class="step_index">3.</div>Укажите параметры:</div>
@@ -129,22 +129,22 @@ while($calc_type = $calc_types->GetNextElement()){
 					<div class="column">
 						<div class="sel_param_row sel_padding">
 							<div class="label_param_betwin"><span class="label_param">Штроба 2х2 см. штроборезом<br>с пылесосом по стене:</span></div>
-							<div><input type="text" class="changed_param" id="5376_wall" value="0"></div>
+							<div><input type="text" class="changed_param" id="19_wall" value="0"></div>
 							<span class="span_mark">&nbsp;в&nbsp;метрах</span>
 						</div>
 						<div class="sel_param_row sel_padding" id="ceiling_display" style="display: none;">
 							<div class="label_param_betwin"><span class="label_param">Штроба 2х2 см. штроборезом<br>с пылесосом по потолку:</span></div>
-							<div><input type="text" class="changed_param" id="5376_ceiling" value="0"></div>
+							<div><input type="text" class="changed_param" id="19_ceiling" value="0"></div>
 							<span class="span_mark">&nbsp;в&nbsp;метрах</span>
 						</div>
 						<div class="sel_param_row sel_padding">
 							<div class="label_param_betwin"><span class="label_param">Гнездо-подрозетника:</span></div>
-							<div><input type="text" class="changed_param" id="5376_socket" value="0"></div>
+							<div><input type="text" class="changed_param" id="19_socket" value="0"></div>
 							<span class="span_mark">&nbsp;штук</span>
 						</div>
 						<div class="sel_param_row sel_padding">
 							<div class="label_param_betwin"><span class="label_param">Штроба под кондиционер:</span></div>
-							<div><input type="text" class="changed_param" id="5376_cond" value="0"></div>
+							<div><input type="text" class="changed_param" id="19_cond" value="0"></div>
 							<span class="span_mark">&nbsp;в метрах</span>
 						</div>
 					</div>
@@ -291,7 +291,7 @@ while($calc_type = $calc_types->GetNextElement()){
 			$('[name="material"]').on('change', function(event){
 				$('[name="param"]').hide();
 				$('[name="param"][work="'+$('[name="work"]:checked').val()+'"]').show();
-				if(parseInt($('[name="material"]:checked').val()) == 5380){
+				if(parseInt($('[name="material"]:checked').val()) == 16){
 					$('#ceiling_display').show();
 				}else{
 					$('#ceiling_display').hide();
@@ -308,40 +308,40 @@ while($calc_type = $calc_types->GetNextElement()){
 			
 			$('input[name="material"]').click(function(){
 				switch($(this).val()){
-					case '5371':
-						$('#5374_thick').empty();
+					case '10':
+						$('#17_thick').empty();
 						var obj = oTypeParam[$(this).val()];
 						for (var key in obj){
 							var key_z = key.replace(/\*/g, '');
-							$('#5374_thick').append('<option value="'+ obj[key] + '">' + key_z + '</option>');
+							$('#17_thick').append('<option value="'+ obj[key] + '">' + key_z + '</option>');
 						}
 						break;
-					case '5373':
-						$('#5374_thick').empty();
+					case '12':
+						$('#17_thick').empty();
 						var obj = oTypeParam[$(this).val()];
 						for (var key in obj){
 							var key_z = key.replace(/\*/g, '');
-							$('#5374_thick').append('<option value="'+ obj[key] + '">' + key_z + '</option>');
+							$('#17_thick').append('<option value="'+ obj[key] + '">' + key_z + '</option>');
 						}
 						break;
-					case '5377':
-						$('#5375_diameter').empty();
+					case '13':
+						$('#18_diameter').empty();
 						var obj = oTypeParam[$(this).val()];
 						for (var key in obj){
 							var key_z = key.replace(/\*/g, '');
-							$('#5375_diameter').append('<option value="'+ obj[key] + '">' + key_z + '</option>');
+							$('#18_diameter').append('<option value="'+ obj[key] + '">' + key_z + '</option>');
 						}
 						break;
-					case '5378':
-						$('#5375_diameter').empty();
+					case '14':
+						$('#18_diameter').empty();
 						var obj = oTypeParam[$(this).val()];
 						for (var key in obj){
 							var key_z = key.replace(/\*/g, '');
-							$('#5375_diameter').append('<option value="'+ obj[key] + '">' + key_z + '</option>');
+							$('#18_diameter').append('<option value="'+ obj[key] + '">' + key_z + '</option>');
 						}
 						break;
-					case '5379':
-					case '5380':
+					case '15':
+					case '16':
 					default:
 				}
 			});
@@ -364,10 +364,10 @@ while($calc_type = $calc_types->GetNextElement()){
                 //console.log('Суммарный коэффициент - ' + kf_result);
 
                 switch($('[name="work"]:checked').val()){
-                    case '5374':
-                        var thick = (parseFloat($('#5374_thick').find('option:selected').text()) > 0) ? parseFloat($('#5374_thick').find('option:selected').text()) : 0;
-                        var price = (parseFloat($('#5374_thick').find('option:selected').val()) > 0) ? parseFloat($('#5374_thick').find('option:selected').val()) : 0;
-                        var length = (parseFloat($('#5374_length').val()) > 0) ? parseFloat($('#5374_length').val()) : 0;
+                    case '17':
+                        var thick = (parseFloat($('#17_thick').find('option:selected').text()) > 0) ? parseFloat($('#17_thick').find('option:selected').text()) : 0;
+                        var price = (parseFloat($('#17_thick').find('option:selected').val()) > 0) ? parseFloat($('#17_thick').find('option:selected').val()) : 0;
+                        var length = (parseFloat($('#17_length').val()) > 0) ? parseFloat($('#17_length').val()) : 0;
 
                         summ = length * price;
                         
@@ -390,11 +390,11 @@ while($calc_type = $calc_types->GetNextElement()){
                         }
                         
                         break;
-                    case '5375':
-                        var diameter = (parseFloat($('#5375_diameter').find('option:selected').text()) > 0) ? parseFloat($('#5375_diameter').find('option:selected').text()) : 0;
-                        var price = (parseFloat($('#5375_diameter').find('option:selected').val()) > 0) ? parseFloat($('#5375_diameter').find('option:selected').val()) : 0;
-                        var depth = (parseFloat($('#5375_depth').val()) > 0) ? $('#5375_depth').val() : 0;
-                        var count = (parseInt($('#5375_count').val()) > 0) ? $('#5375_count').val() : 0;
+                    case '18':
+                        var diameter = (parseFloat($('#18_diameter').find('option:selected').text()) > 0) ? parseFloat($('#18_diameter').find('option:selected').text()) : 0;
+                        var price = (parseFloat($('#18_diameter').find('option:selected').val()) > 0) ? parseFloat($('#18_diameter').find('option:selected').val()) : 0;
+                        var depth = (parseFloat($('#18_depth').val()) > 0) ? $('#18_depth').val() : 0;
+                        var count = (parseInt($('#18_count').val()) > 0) ? $('#18_count').val() : 0;
                         
                         if (price > 0){
                             summ = depth * count * price;
@@ -439,13 +439,13 @@ while($calc_type = $calc_types->GetNextElement()){
                         }
                         
                         break;
-                    case '5376':    // ШТРОБЛЕНИЕ
-                        var wall = (parseFloat($('#5376_wall').val()) > 0) ? $('#5376_wall').val() : 0;
-                        var socket = (parseInt($('#5376_socket').val()) > 0) ? $('#5376_socket').val() : 0;
-                        var cond = (parseInt($('#5376_cond').val()) > 0) ? $('#5376_cond').val() : 0;
+                    case '19':    // ШТРОБЛЕНИЕ
+                        var wall = (parseFloat($('#19_wall').val()) > 0) ? $('#19_wall').val() : 0;
+                        var socket = (parseInt($('#19_socket').val()) > 0) ? $('#19_socket').val() : 0;
+                        var cond = (parseInt($('#19_cond').val()) > 0) ? $('#19_cond').val() : 0;
                         
-                        if(parseInt($('[name="material"]:checked').val()) == 5380){
-                            var ceiling = (parseFloat($('#5376_ceiling').val()) > 0) ? $('#5376_ceiling').val() : 0;
+                        if(parseInt($('[name="material"]:checked').val()) == 16){
+                            var ceiling = (parseFloat($('#19_ceiling').val()) > 0) ? $('#19_ceiling').val() : 0;
                             summ = (
                                     (wall * oTypeParam[$('[name="material"]:checked').val()]['1*']) + 
                                     (ceiling * oTypeParam[$('[name="material"]:checked').val()]['2*']) +
